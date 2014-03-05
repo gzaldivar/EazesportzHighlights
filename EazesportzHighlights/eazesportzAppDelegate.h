5b3b3f479e7fa67dd802fa99638dc6e6c4f4639c
Gil Zaldivar <gilcycle@gmail.com>
2014-03-04 21:10:49 -0800

    Initial Commit

---
+++ EazesportzHighlights/eazesportzAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  eazesportzAppDelegate.h
+//  EazesportzHighlights
+//
+//  Created by Gilbert Zaldivar on 3/4/14.
+//  Copyright (c) 2014 Gilbert Zaldivar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface eazesportzAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ EazesportzHighlights/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EazesportzHighlights
+//
+//  Created by Gilbert Zaldivar on 3/4/14.
+//  Copyright (c) 2014 Gilbert Zaldivar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
